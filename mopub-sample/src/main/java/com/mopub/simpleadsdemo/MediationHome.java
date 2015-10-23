bafbf74669c4beb41e648946653cc1eb98362df1
shmeadyy <lauraemead7@gmail.com>
2015-10-23 16:54:42 -0700

    initialized radio group logic

--- mopub-sample/src/main/java/com/mopub/simpleadsdemo/MediationHome.java
+++ mopub-sample/src/main/java/com/mopub/simpleadsdemo/MediationHome.java
@@ -5,10 +5,11 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
+import android.widget.RadioGroup;
 
 
 
@@ -26,6 +27,8 @@ public class MediationHome extends Fragment {
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
+
+    private RadioGroup adFormatRadioGroup;
 
     // TODO: Rename and change types of parameters
     private String mParam1;
@@ -58,10 +61,20 @@ public class MediationHome extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Activity.setContentView(R.layout.fragment_mediation_home);
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+        // Initialize Radio Group and attach click handler
+        adFormatRadioGroup = (RadioGroup) findViewById(R.id.adFormatRadioGroup);
+        adFormatRadioGroup.clearCheck();
+    }
+
+    public void onClear(View v) {
+        // Clears all selected radio buttons to default
+        adFormatRadioGroup.clearCheck();
     }
 
     @Override
